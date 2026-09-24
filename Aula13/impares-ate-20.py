7f33b53a32657a244fcf40e18adde89820b6eee8
jacob-derek <jacobderekprf@gmail.com>
2026-09-24 14:41:38 -0300

    mais atividades

---
+++ Aula13/impares-ate-20.py
@@ -0,0 +1,8 @@
+#impares ate 20
+
+for i in range(21):
+    if i % 2 != 0:
+        print(f"{i} é impar")
+    else:
+        pass
+
---
+++ Aula13/somando.py
@@ -0,0 +1,8 @@
+#somar todos os algarismos
+
+user_num = int(input("Digite um numero: "))
+acumulador = 0
+
+for i in range(user_num + 1):
+    a += i
+    print(acumulador)
